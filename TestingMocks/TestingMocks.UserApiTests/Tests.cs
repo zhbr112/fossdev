27814d8f55f6ea86c45c63f077e93e74416d1cf9
zhbr112 <bigrashinboss1@gmail.com>
2025-02-15 00:26:16 +0800

    Создал начальный проект

---
+++ TestingMocks/TestingMocks.UserApiTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace TestingMocks.UserApiTests;
+
+public class Tests
+{
+    [ClassDataSource<WebApplicationFactory>(Shared = SharedType.PerTestSession)]
+    public required WebApplicationFactory WebApplicationFactory { get; init; }
+
+    [Test]
+    public async Task Test()
+    {
+        var client = WebApplicationFactory.CreateClient();
+
+        var response = await client.GetAsync("/");
+
+        var stringContent = await response.Content.ReadAsStringAsync();
+
+        await Assert.That(stringContent).IsEqualTo("0");
+    }
+}
