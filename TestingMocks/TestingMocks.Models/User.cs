dc2106691bec9e284a098e23a920a17673cb610c
zhbr112 <bigrashinboss1@gmail.com>
2025-02-15 18:24:36 +0800

    Добавил модель пользователя

---
+++ TestingMocks/TestingMocks.Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingMocks.Models;
+
+public class User
+{
+    [Key, StringLength(32, MinimumLength = 3)]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
