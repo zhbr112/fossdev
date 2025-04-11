e8a4ebb65c4127b7de2dd76dbb92b193e5adc26a
zhbr112 <bigrashinboss1@gmail.com>
2025-04-11 18:27:40 +0800

    Сделал тестирование ELK

---
+++ ELK/ElkTest/Models/User.cs
@@ -0,0 +1,9 @@
+namespace ElkTest.Models;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
